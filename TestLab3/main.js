79128268c6dbe808ccdfe1e4f81f49600a576cfb
jeslinthomas <144937184+jeslinthomas@users.noreply.github.com>
2023-10-27 14:28:17 -0500

    Update main.js

--- TestLab3/main.js
+++ TestLab3/main.js
@@ -6,33 +6,21 @@ function billFormat(value) {
   return '$' + value;
 }
 
-function splitFormat(value) {
-  if (value == 1 ) return value + ' Person';
-  return value + ' People';
-}
-
 function yourBill() {
-  var bill = Number(document.getElementById ('yourBill').value);
-  if(isNaN(bill) || bill <= 0)
-  {
-      alert("Numbers Only Please!");
-      document.getElementById('yourBill').value = '';
-      yourBill = "";
+  var bill = Number(document.getElementById('yourBill').value);
+  if (isNaN(bill) || bill <= 0) {
+    alert("Numbers Only Please!");
+    document.getElementById('yourBill').value = '';
+    return;
   }
-  var tipPercent = document.getElementById ('tipInput').value;
-  var split = document.getElementById ('splitInput').value;
-  var tipValue = bill * (tipPercent / 100) ;   // console.log(tipValue);
-  var tipEach = tipValue / split;
-  var splitBillEach = ( bill + tipValue ) / split;
-  console.log(splitBillEach);
+  var tipPercent = document.getElementById('tipInput').value;
+  var tipValue = (bill * tipPercent) / 100;
+  var totalWithTip = bill + tipValue;
 
-  document.getElementById('tipPercent').innerHTML = tipPercent + '%';
-  document.getElementById('tipValue').innerHTML = billFormat(tipValue);
-  document.getElementById('totalWithTip').innerHTML = billFormat(bill + tipValue);
-  document.getElementById('splitValue').innerHTML = splitFormat(split);
-  document.getElementById('billEach').innerHTML = billFormat(splitBillEach);
-  document.getElementById('tipEach').innerHTML = billFormat(tipEach);
-
+  document.getElementById('tipPercent').textContent = tipPercent + '%';
+  document.getElementById('tipValue').textContent = billFormat(tipValue);
+  document.getElementById('totalWithTip').textContent = billFormat(totalWithTip);
 }
-  var oninput = document.getElementById('oninput');
-  oninput.addEventListener('input', yourBill)
+
+var oninput = document.getElementById('tipCalculator');
+oninput.addEventListener('input', yourBill);
