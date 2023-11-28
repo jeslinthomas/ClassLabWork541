4d01f5a65619134ccf918f786a57567f3a2ed552
jeslinthomas <144937184+jeslinthomas@users.noreply.github.com>
2023-11-28 11:29:25 -0600

    Update script.js

--- TestLab4/script.js
+++ TestLab4/script.js
@@ -1,111 +1,119 @@
-// script.js
-function getSunriseSunsetData(latitude, longitude) {
-  const apiUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&date=today&formatted=0`;
-
-  $.ajax({
-    url: apiUrl,
-    method: 'GET',
-    success: function (data) {
-      updateDashboard(data.results);
-      // Fetch and update tomorrow's data after updating today's data
-      fetchTomorrowData(latitude, longitude);
-    },
-    error: function (error) {
-      handleApiError(error.responseJSON);
-    },
-  });
-}
-
-function fetchTomorrowData(latitude, longitude) {
-  const tomorrowDate = new Date();
-  tomorrowDate.setDate(tomorrowDate.getDate() + 1);
-
-  const tomorrowApiUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&date=${tomorrowDate.toISOString().split('T')[0]}&formatted=0`;
-
-  $.ajax({
-    url: tomorrowApiUrl,
-    method: 'GET',
-    success: function (data) {
-      updateTomorrowData(data.results);
-    },
-    error: function (error) {
-      handleApiError(error.responseJSON);
-    },
-  });
-}
-
-function updateDashboard(results) {
-  $('#today-sunrise').text(results.sunrise);
-  $('#today-sunset').text(results.sunset);
-  $('#today-dawn').text(results.civil_twilight_begin);
-  $('#today-dusk').text(results.civil_twilight_end);
-  $('#today-day-length').text(results.day_length);
-  $('#today-solar-noon').text(results.solar_noon);
-  $('#timezone').text(results.timezone);
-}
-
-function updateTomorrowData(results) {
-  $('#tomorrow-sunrise').text(results.sunrise);
-  $('#tomorrow-sunset').text(results.sunset);
-  $('#tomorrow-dawn').text(results.civil_twilight_begin);
-  $('#tomorrow-dusk').text(results.civil_twilight_end);
-  $('#tomorrow-day-length').text(results.day_length);
-  $('#tomorrow-solar-noon').text(results.solar_noon);
-}
-
-function handleApiError(error) {
-  $('#error-message').text(`Error: ${error.status} - ${error.message}`);
-  $('#error-message').show();
-
-  // Clear previous data
-  $('.sun-info').text('');
-}
-
-$('#search-btn').on('click', function () {
-  const locationName = $('#location-input').val();
-  if (locationName.trim() !== '') {
-    const geocodeUrl = `https://geocode.maps.co/?address=${encodeURIComponent(locationName)}`;
-
-    $.ajax({
-      url: geocodeUrl,
-      method: 'GET',
-      success: function (data) {
-        if (data.results.length > 0) {
-          const latitude = data.results[0].geometry.location.lat;
-          const longitude = data.results[0].geometry.location.lng;
-          getSunriseSunsetData(latitude, longitude);
-        } else {
-          handleApiError({ status: 404, message: 'Location not found.' });
-        }
-      },
-      error: function (error) {
-        handleApiError(error.responseJSON);
-      },
-    });
-  } else {
-    handleApiError({ status: 400, message: 'Please enter a location.' });
-  }
+// Event listener for using geolocation 
+document.getElementById('geoLocation').addEventListener('click', function() {
+    resetInputs();
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            getSunriseSunsetInfo(position.coords.latitude, position.coords.longitude);
+        }, function(error) {
+            alert("Error in Geolocation: " + error.message);
+        });
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
 });
 
-$('#current-location-btn').on('click', function () {
-  getCurrentLocation();
+// Event listener for selecting a predefined location
+document.getElementById('presetLocations').addEventListener('change', function() {
+    resetInputs('select');
+    const coords = this.value.split(',');
+    if (coords.length === 2) {
+        getSunriseSunsetInfo(coords[0], coords[1]);
+    }
 });
 
-function getCurrentLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      function (position) {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        getSunriseSunsetData(latitude, longitude);
-      },
-      function (error) {
-        handleApiError({ status: error.code, message: error.message });
-      }
-    );
-  } else {
-    handleApiError({ status: 0, message: 'Geolocation is not supported by this browser.' });
-  }
+// Event listener for the search location input
+document.getElementById('searchLocation').addEventListener('input', function() {
+    resetInputs('input'); // Reset other inputs
+});
+
+// Event listener for searching a location
+document.getElementById('searchButton').addEventListener('click', function() {
+    const location = document.getElementById('searchLocation').value;
+    if (location) {
+        fetch(`https://geocode.maps.co/search?q=${encodeURIComponent(location)}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data && data.length > 0) {
+                    getSunriseSunsetInfo(data[0].lat, data[0].lon);
+                } else {
+                    alert("Location not found.");
+                }
+            })
+            .catch(() => alert("Error fetching location data."));
+    } else {
+        alert("Please enter a location to search.");
+    }
+});
+
+// Function to reset other input methods
+function resetInputs(triggeredBy = '') {
+    if (triggeredBy !== 'select') {
+        document.getElementById('presetLocations').value = '';
+    }
+    if (triggeredBy !== 'input') {
+        document.getElementById('searchLocation').value = '';
+    }
 }
 
-getCurrentLocation();
+// Function to fetch sunrise and sunset info
+function getSunriseSunsetInfo(latitude, longitude) {
+    // Fetch data for today
+    fetchSunriseSunsetData(latitude, longitude, 'today')
+        .then(dataToday => {
+            // Fetch data for tomorrow
+            fetchSunriseSunsetData(latitude, longitude, 'tomorrow')
+                .then(dataTomorrow => {
+                    updateDisplay(dataToday, dataTomorrow);
+                })
+                .catch(() => alert("Error fetching tomorrow's sunrise and sunset data."));
+        })
+        .catch(() => alert("Error fetching today's sunrise and sunset data."));
+}
+
+// Helper function to fetch sunrise and sunset data
+function fetchSunriseSunsetData(latitude, longitude, date) {
+    return fetch(`https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&date=${date}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'OK') {
+                return data.results;
+            } else {
+                throw new Error("API Error");
+            }
+        });
+}
+
+// Helper function to format UTC offset
+function formatUTCOffset(offsetMinutes) {
+    const hours = Math.floor(Math.abs(offsetMinutes) / 60);
+    const minutes = Math.abs(offsetMinutes) % 60;
+    const sign = offsetMinutes >= 0 ? '+' : '-';
+    return `${sign}${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+}
+
+function updateDisplay(dataToday, dataTomorrow) {
+    const todayData = document.getElementById('todayData');
+    const tomorrowData = document.getElementById('tomorrowData');
+    
+    todayData.innerHTML = `
+        <strong>Today:</strong>
+        <div>Sunrise: ${dataToday.sunrise}</div>
+        <div>Sunset: ${dataToday.sunset}</div>
+        <div>Dawn: ${dataToday.dawn}</div>
+        <div>Dusk: ${dataToday.dusk}</div>
+        <div>Day Length: ${dataToday.day_length}</div>
+        <div>Solar Noon: ${dataToday.solar_noon}</div>
+        <div>Timezone: ${dataToday.timezone} (UTC ${formatUTCOffset(dataToday.utc_offset)})</div>
+    `;
+
+    tomorrowData.innerHTML = `
+        <strong>Tomorrow:</strong>
+        <div>Sunrise: ${dataTomorrow.sunrise}</div>
+        <div>Sunset: ${dataTomorrow.sunset}</div>
+        <div>Dawn: ${dataTomorrow.dawn}</div>
+        <div>Dusk: ${dataTomorrow.dusk}</div>
+        <div>Day Length: ${dataTomorrow.day_length}</div>
+        <div>Solar Noon: ${dataTomorrow.solar_noon}</div>
+        <div>Timezone: ${dataTomorrow.timezone} (UTC ${formatUTCOffset(dataTomorrow.utc_offset)})</div>
+    `;
+}
