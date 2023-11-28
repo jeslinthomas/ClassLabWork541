d48511214de82716af59cd5c4a3ac140af43a04c
jeslinthomas <144937184+jeslinthomas@users.noreply.github.com>
2023-11-27 21:34:24 -0600

    Update script.js

--- Lab4/script.js
+++ Lab4/script.js
@@ -1,168 +1,119 @@
-let weather={
-  fetchweather: function(city){
-    fetch("https://weatherdbi.herokuapp.com/data/weather/"+city)
-    .then((response)=>{
-      
-      if (!response.ok) {
-        document.querySelector(".error").innerText="Please check the city name for getting weather details...";
-        alert("No weather found.");
-        throw new Error("No weather found.");
-      }
-      
-      return response.json();
-      
-    })
-    .then((data)=>this.displayWeather(data)); 
-   },
-
-   displayWeather: function(data){
-    if(data.code==0)
-    {
-      document.querySelector(".error").innerText="The specified locatin does not exist. Please check the city name.";
-     // alert(data.message); 
-      //throw new Error("No weather found.");
-    }
-    if(data.code==1)
-    {
-      document.querySelector(".error").innerText="Please do not enter any special characters while fetching weather details";
-      //alert(data.message); 
-      //throw new Error("No weather found.");
-    }
-    if(data.code==2)
-    {
-      document.querySelector(".error").innerText="Search by coordinates not available due to excessive use of this feature. Try after sometime";
-     // alert(data.message); 
-      //throw new Error("No weather found.");
-    }
-  
-    const { region }=data;
-    const { dayhour }=data.currentConditions;
-    //const { temp }=data.currentConditions;s
-    const { c }=data.currentConditions.temp;
-    const { precip }=data.currentConditions;
-    const { humidity }=data.currentConditions;
-    //const { wind }=data.currentConditions;
-    const { km }=data.currentConditions.wind;
-
-    const { comment }=data.currentConditions;
-    const { iconURL }=data.currentConditions;
-    console.log(region,dayhour,c,precip,humidity,km,comment,iconURL);
-    document.querySelector(".error").innerText="";
-    document.querySelector(".city").innerText="Weather in "+ region + " at "+dayhour;
-    document.querySelector(".icon").src=iconURL;
-    document.querySelector(".temp").innerText=c+"°C";
-    document.querySelector(".description").innerText=comment;
-    document.querySelector(".humidity").innerText="Humidity: "+humidity;
-    document.querySelector(".wind").innerText="Wind Speed: "+km+" km/h";
-    document.querySelector(".weather").classList.remove("loading");
-
-    for(i=0;i<7;i++)
-    { 
-      //document.querySelector("day" + (i) + "max").innerText = "Max: " + data.next_days[0].max_temp+ "°C";
-      document.querySelector(".weather_forecast_day"+(i+1)).innerText=data.next_days[i].day; 
-      document.querySelector(".description"+(i+1)).innerText=data.next_days[i].comment;  
-      document.querySelector(".day"+(i+1)+"max").innerText="Max: "+data.next_days[i].max_temp.c;  
-      document.querySelector(".day"+(i+1)+"min").innerText="Min: "+data.next_days[i].min_temp.c;  
-      document.querySelector(".weather_forecast_icon"+(i+1)).src=data.next_days[i].iconURL;  
-
-    }
-
-    document.body.style.backgroundImage =
-    "url('https://source.unsplash.com/1600x900/?" + region + "')";
-
-   },
-
-   search: function(){
-    this.fetchweather(document.querySelector(".search-bar").value);
-   } 
-   
-};
-
-let geocode = {
-  reverseGeocode: function (latitude, longitude) {
-    var apikey = "80a0a5c1c1db477e8567458eda7327ee";
-
-    var api_url = "https://api.opencagedata.com/geocode/v1/json";
-
-    var request_url =
-      api_url +
-      "?" +
-      "key=" +
-      apikey +
-      "&q=" +
-      encodeURIComponent(latitude + "," + longitude) +
-      "&pretty=1" +
-      "&no_annotations=1";
-
-    // see full list of required and optional parameters:
-    // https://opencagedata.com/api#forward
-
-    var request = new XMLHttpRequest();
-    request.open("GET", request_url, true);
-
-    request.onload = function () {
-      // see full list of possible response codes:
-      // https://opencagedata.com/api#codes
-
-      if (request.status == 200) {
-        // Success!
-        var data = JSON.parse(request.responseText);
-        weather.fetchweather(data.results[0].components.city);
-        console.log(data.results[0].components.city)
-      } else if (request.status <= 500) {
-        // We reached our target server, but it returned an error
-
-        console.log("unable to geocode! Response code: " + request.status);
-        var data = JSON.parse(request.responseText);
-        console.log("error msg: " + data.status.message);
-      } else {
-        console.log("server error");
-      }
-    };
-
-    request.onerror = function () {
-      // There was a connection error of some sort
-      console.log("unable to connect to server");
-    };
-
-    request.send(); // make the request
-  },
-  getLocation: function() {
-    function success (data) {
-      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
-    }
+// Event listener for utilizing geolocation
+document.getElementById('geoLocation').addEventListener('click', function() {
+    resetInputs();
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, console.error);
+        navigator.geolocation.getCurrentPosition(function(position) {
+            fetchSunriseSunsetInfo(position.coords.latitude, position.coords.longitude);
+        }, function(error) {
+            alert("Error in Geolocation: " + error.message);
+        });
+    } else {
+        alert("Geolocation is not supported by this browser.");
     }
-    else {
-      weather.fetchWeather("Denver");
-    }
-  }
-};
-
-document.querySelector(".search button").addEventListener("click",function(){
-  weather.search();
-})
-
-document.querySelector(".search-bar").addEventListener("keyup",function(event){
-  if(event.key=="Enter")
-  weather.search();
 });
 
-document.querySelector(".geolocation button").addEventListener("click", () =>{
-    document.querySelector(".search-bar").value="";
-    geocode.getLocation();
-  
+// Event listener for selecting a predefined location
+document.getElementById('presetLocations').addEventListener('change', function() {
+    resetInputs('select');
+    const coordinates = this.value.split(',');
+    if (coordinates.length === 2) {
+        fetchSunriseSunsetInfo(coordinates[0], coordinates[1]);
+    }
 });
 
+// Event listener for the search location input
+document.getElementById('searchLocation').addEventListener('input', function() {
+    resetInputs('input'); // Reset other inputs
+});
 
+// Event listener for searching a location
+document.getElementById('searchButton').addEventListener('click', function() {
+    const locationValue = document.getElementById('searchLocation').value;
+    if (locationValue) {
+        fetch(`https://geocode.maps.co/search?q=${encodeURIComponent(locationValue)}`)
+            .then(response => response.json())
+            .then(locationData => {
+                if (locationData && locationData.length > 0) {
+                    fetchSunriseSunsetInfo(locationData[0].lat, locationData[0].lon);
+                } else {
+                    alert("Location not found.");
+                }
+            })
+            .catch(() => alert("Error fetching location data."));
+    } else {
+        alert("Please enter a location to search.");
+    }
+});
 
-function onError(error){
-  // if any error occur while getting user location then we'll show it in infoText
-  //infoTxt.innerText = error.message;
-  //infoTxt.classList.add("error");
-  document.querySelector(".error").innerText="There was an error fetching user location from the device..."
-  console.log(error.message);
+// Function to reset other input methods
+function resetInputs(triggeredBy = '') {
+    if (triggeredBy !== 'select') {
+        document.getElementById('presetLocations').value = '';
+    }
+    if (triggeredBy !== 'input') {
+        document.getElementById('searchLocation').value = '';
+    }
 }
 
-weather.fetchweather("Kansas");
+// Function to fetch sunrise and sunset info
+function fetchSunriseSunsetInfo(latitude, longitude) {
+    // Fetch data for today
+    fetchSunriseSunsetData(latitude, longitude, 'today')
+        .then(dataToday => {
+            // Fetch data for tomorrow
+            fetchSunriseSunsetData(latitude, longitude, 'tomorrow')
+                .then(dataTomorrow => {
+                    updateDisplay(dataToday, dataTomorrow);
+                })
+                .catch(() => alert("Error fetching tomorrow's sunrise and sunset data."));
+        })
+        .catch(() => alert("Error fetching today's sunrise and sunset data."));
+}
+
+// Helper function to fetch sunrise and sunset data
+function fetchSunriseSunsetData(latitude, longitude, date) {
+    return fetch(`https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&date=${date}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'OK') {
+                return data.results;
+            } else {
+                throw new Error("API Error");
+            }
+        });
+}
+
+// Helper function to format UTC offset
+function formatUTCOffset(offsetMinutes) {
+    const hours = Math.floor(Math.abs(offsetMinutes) / 60);
+    const minutes = Math.abs(offsetMinutes) % 60;
+    const sign = offsetMinutes >= 0 ? '+' : '-';
+    return `${sign}${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+}
+
+function updateDisplay(dataToday, dataTomorrow) {
+    const todayData = document.getElementById('todayData');
+    const tomorrowData = document.getElementById('tomorrowData');
+    
+    todayData.innerHTML = `
+        <strong>Today:</strong>
+        <div>Sunrise: ${dataToday.sunrise}</div>
+        <div>Sunset: ${dataToday.sunset}</div>
+        <div>Dawn: ${dataToday.dawn}</div>
+        <div>Dusk: ${dataToday.dusk}</div>
+        <div>Day Length: ${dataToday.day_length}</div>
+        <div>Solar Noon: ${dataToday.solar_noon}</div>
+        <div>Timezone: ${dataToday.timezone} (UTC ${formatUTCOffset(dataToday.utc_offset)})</div>
+    `;
+
+    tomorrowData.innerHTML = `
+        <strong>Tomorrow:</strong>
+        <div>Sunrise: ${dataTomorrow.sunrise}</div>
+        <div>Sunset: ${dataTomorrow.sunset}</div>
+        <div>Dawn: ${dataTomorrow.dawn}</div>
+        <div>Dusk: ${dataTomorrow.dusk}</div>
+        <div>Day Length: ${dataTomorrow.day_length}</div>
+        <div>Solar Noon: ${dataTomorrow.solar_noon}</div>
+        <div>Timezone: ${dataTomorrow.timezone} (UTC ${formatUTCOffset(dataTomorrow.utc_offset)})</div>
+    `;
+}
